08da3fc2bf9503e77466f1ea977f482c0612aa80
James Dixon <notjamesdixon@gmail.com>
2020-05-12 14:44:32 -0400

    add vimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+[ ! -d "$HOME/.vim/" ] && mkdir "$HOME/.vim"
+
 cp bash/bashrc        ~/.bashrc
 cp bash/bash_profile  ~/.bash_profile
 cp tmux/tmux.conf     ~/.tmux.conf
 cp git/gitconfig      ~/.gitconfig
+cp vim/vimrc          ~/.vim/vimrc
