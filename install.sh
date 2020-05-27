86834a0fefa84e6028237b011c158aed20d12d92
James Dixon <notjamesdixon@gmail.com>
2020-05-27 13:06:49 -0400

    add scripts dir

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-[ ! -d "$HOME/.vim/" ] && mkdir "$HOME/.vim"
+# make these homey paths
+mkdir -pv "$HOME/.vim" "$HOME/.local/"{bin,lib} "$HOME/.config"
 
-cp bash/bashrc        ~/.bashrc
-cp bash/bash_profile  ~/.bash_profile
-cp tmux/tmux.conf     ~/.tmux.conf
-cp git/gitconfig      ~/.gitconfig
-cp vim/vimrc          ~/.vim/vimrc
+# find our dotfiles repo
+REPOPATH=$(find "$HOME" -type d -iname 'dotfiles')
+
+# copy our files
+cp "$REPOPATH/bash/bashrc"        "$HOME/.bashrc"
+cp "$REPOPATH/bash/bash_profile"  "$HOME/.bash_profile"
+cp "$REPOPATH/tmux/tmux.conf"     "$HOME/.tmux.conf"
+cp "$REPOPATH/git/gitconfig"      "$HOME/.gitconfig"
+cp "$REPOPATH/vim/vimrc"          "$HOME/.vim/vimrc"
+
+# link scripts dir
+ln -sv "$REPOPATH/scripts" "$HOME/.local/"
+
