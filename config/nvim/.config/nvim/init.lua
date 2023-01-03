1105e45c4b7e74ed0a864fd01fa79a27be470ab3
James Dixon <notjamesdixon@gmail.com>
2023-01-03 11:29:36 -0500

    add win32 clipboard hack

--- config/nvim/.config/nvim/init.lua
+++ config/nvim/.config/nvim/init.lua
@@ -11,3 +11,17 @@ require('plugins')
 
 -- plugin configurations
 require('plugin-config')
+
+-- platform/os specific stuff
+if vim.fn.has('win32') then
+  vim.cmd([[
+    " WSL yank support
+    let s:clip = '/mnt/c/Windows/System32/clip.exe'  " change this path according to your mount point
+    if executable(s:clip)
+        augroup WSLYank
+            autocmd!
+            autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
+        augroup END
+    endif
+  ]])
+end
